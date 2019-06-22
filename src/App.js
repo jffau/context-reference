885a9886f94e6cb2e9989e79f24d5ae011782eac
jeffau13 <jeffau13@gmail.com>
2019-06-22 18:25:09 -0400

    init

--- src/App.js
+++ src/App.js
@@ -1,26 +1,85 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// first we will make a new context
+const MyContext = React.createContext();
+
+// Then create a provider Component
+class MyProvider extends Component {
+  state = {
+    name: 'Person',
+    age: 100,
+    cool: true
+  };
+  render() {
+    return (
+      <MyContext.Provider
+        value={{
+          state: this.state,
+          growAYearOlder: () => {
+            this.setState({
+              age: this.state.age + 1
+            });
+          },
+          handleChange: event => {
+            this.setState({
+              name: event.target.value
+            });
+          }
+        }}
+      >
+        {this.props.children}
+      </MyContext.Provider>
+    );
+  }
+}
+
+const Family = props => (
+  <div className="family">
+    <Person />
+  </div>
+);
+
+class Person extends Component {
+  render() {
+    return (
+      <div className="person">
+        <MyContext.Consumer>
+          {context => (
+            <React.Fragment>
+              <p>Age: {context.state.age}</p>
+              <p>Name: {context.state.name}</p>
+              <button onClick={context.growAYearOlder}>
+                Grow a year older
+              </button>
+              <form>
+                <label>
+                  Change Name:
+                  <input
+                    type="text"
+                    value={context.state.name}
+                    onChange={context.handleChange}
+                  />
+                </label>
+              </form>
+            </React.Fragment>
+          )}
+        </MyContext.Consumer>
+      </div>
+    );
+  }
+}
+
+class App extends Component {
+  render() {
+    return (
+      <MyProvider>
+        <div>
+          <p>I am the app</p>
+          <Family />
+        </div>
+      </MyProvider>
+    );
+  }
 }
 
 export default App;
